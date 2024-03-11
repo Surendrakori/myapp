42797eb505d0abec5abef3d4fe52c855755bde81
surendra <surendrakori@lerostech.com>
2024-03-11 13:59:53 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginForm from './LoginForm';
-import App16 from './App16';
 import sample from './sample';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
